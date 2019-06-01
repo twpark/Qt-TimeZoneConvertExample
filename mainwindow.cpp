a8efed5b37bd593b2561043d0303403ba4e240d6
Taiwoo Park <twp@spu.edu>
2019-05-31 23:53:41 -0700

    initial commit -2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,51 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QDateTime>
+#include <QTime>
+#include <QTimeZone>
+#include <QDebug>
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    timeZoneTest();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::timeZoneTest()
+{
+    // Getting the current time
+    QDateTime dateTime = QDateTime::currentDateTime();
+    // Debug: Current time in local timezone
+    qDebug() << dateTime.toString();
+    // Debug: Current time / local timezone, formatted
+    qDebug() << dateTime.toString("hh");
+    qDebug() << dateTime.toString("mm");
+    qDebug() << dateTime.toString("ss");
+
+    // Create a new timezone object to convert
+    QTimeZone newZone("America/Chicago");
+    // Debug: Check if the new zone is valid
+    qDebug() << newZone;
+
+    // Convert the current time to the new timezone
+    QDateTime newDateTime = dateTime.toTimeZone(newZone);
+
+    // Debug: The converted time
+    qDebug() << newDateTime.toString();
+    // Debug: The converted time (formatted: hr, min, sec)
+    qDebug() << newDateTime.toString("hh");
+    qDebug() << newDateTime.toString("mm");
+    qDebug() << newDateTime.toString("ss");
+
+    // To list all available timezone IDs, use the following's output
+    //qDebug() << QTimeZone::availableTimeZoneIds();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,6 +16,7 @@ public:
     ~MainWindow();
 
 private:
+    void timeZoneTest();
     Ui::MainWindow *ui;
 };
 
